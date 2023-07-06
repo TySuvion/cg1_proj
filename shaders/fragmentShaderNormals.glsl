df6516b137e831a00197c16d2e966b3bf6b9e6e4
HeyUKonrad <konrad.m.kappmeyer@gmail.com>
2023-07-06 14:57:44 +0200

    shader können den Teapot erfolgreich mit Schatten versorgen <3

--- shaders/fragmentShaderNormals.glsl
+++ shaders/fragmentShaderNormals.glsl
@@ -1,15 +1,19 @@
 precision mediump float;
 
 uniform vec3 materialColor;
-varying vec3 vNormal;
+varying vec3 fragNormal;
+varying vec3 fragColor;
 
 void main(){
-    vec3 lightDirection = normalize(vec3(0.0, 1.0, 0.0)); //light direction
-    float lightIntensity = dot(vNormal, lightDirection);
 
-    vec3 shadingColor = vec3(lightIntensity);
+    vec3 ambientLightIntensity = vec3(0.1,0.1,0.2);
+    vec3 sunlightIntensity = vec3(0.9,0.8,0.6);
+    vec3 sunlightDirection = normalize(vec3(1.0,4.0,0.0));
 
-    vec3 finalColor = shadingColor * materialColor;
+    vec4 materialColor = vec4(0.8,0.6,0.6,1.0);
 
-    gl_FragColor = vec4(finalColor, 1.0);
+    vec3 lightIntensity = ambientLightIntensity +
+        (sunlightIntensity)* max(dot(fragNormal, sunlightDirection), 0.0);
+
+    gl_FragColor = vec4 (materialColor.rgb * lightIntensity, 1.0);
 }
